b5f34446c3bca6cd330fd9e8c4c5f021df0c3824
Adam Kennedy <adamk@cpan.org>
2009-04-04 03:01:40 +0000

    Changed finalize to reset per RT #32100 et al

--- dbdimp.c
+++ dbdimp.c
@@ -430,8 +430,7 @@ sqlite_st_execute (SV *sth, imp_sth_t *imp_sth)
         case SQLITE_DONE: DBIc_ACTIVE_on(imp_sth);
                           sqlite_trace(5, "exec ok - %d rows, %d cols\n", imp_sth->nrow, DBIc_NUM_FIELDS(imp_sth));
                           return 0;
-                          /* There are bug reports that say this should be sqlite3_reset() */
-        default:          sqlite3_finalize(imp_sth->stmt);
+        default:          sqlite3_reset(imp_sth->stmt);
                           imp_sth->stmt = NULL;
                           sqlite_error(sth, (imp_xxh_t*)imp_sth, imp_sth->retval, (char*)sqlite3_errmsg(imp_dbh->db));
                           return -6;
