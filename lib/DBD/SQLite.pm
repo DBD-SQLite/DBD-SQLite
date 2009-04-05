788904db720695b56b93c66aded2cf037bb8f157
Adam Kennedy <adamk@cpan.org>
2009-04-05 23:43:36 +0000

    Updating version to _11 and adding Changes entries

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -8,7 +8,7 @@ use DynaLoader ();
 use vars qw($VERSION @ISA);
 use vars qw{$err $errstr $drh $sqlite_version};
 BEGIN {
-    $VERSION = '1.19_10';
+    $VERSION = '1.19_11';
     @ISA     = ('DynaLoader');
 
     # Driver singleton
--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -9,7 +9,7 @@ use Test::More ();
 
 use vars qw{$VERSION @ISA @EXPORT};
 BEGIN {
-	$VERSION = '1.19_10';
+	$VERSION = '1.19_11';
 	@ISA     = qw{ Exporter };
 	@EXPORT  = qw{ connect_ok };
 
