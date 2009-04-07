ef56832c32e76e544623e1d63d9e994646dbe9c8
Kenichi Ishigaki <ishigaki@cpan.org>
2009-04-07 18:33:32 +0000

    DBD-SQLite: found there was a better way to return undef...

--- dbdimp.c
+++ dbdimp.c
@@ -350,8 +350,8 @@ sqlite_st_execute (SV *sth, imp_sth_t *imp_sth)
     /* warn("execute\n"); */
 
     if (!DBIc_ACTIVE(imp_dbh)) {
-        sqlite_error(sth, (imp_xxh_t*)imp_sth, (int)&PL_sv_undef, "attempt to execute on inactive database handle");
-        return FALSE;
+        sqlite_error(sth, (imp_xxh_t*)imp_sth, retval, "attempt to execute on inactive database handle");
+        return -2;
     }
 
     if (DBIc_ACTIVE(imp_sth)) {
