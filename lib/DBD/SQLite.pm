0ede9e1124960be6b1206dd1bb6f15e1077939c8
Adam Kennedy <adamk@cpan.org>
2009-04-08 01:16:57 +0000

    Preparing for 1.21 release

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -8,7 +8,7 @@ use DynaLoader ();
 use vars qw($VERSION @ISA);
 use vars qw{$err $errstr $drh $sqlite_version};
 BEGIN {
-    $VERSION = '1.20';
+    $VERSION = '1.21';
     @ISA     = ('DynaLoader');
 
     # Driver singleton
