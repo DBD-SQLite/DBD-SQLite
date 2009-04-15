dfd5fc6f14a70be025f982c85b47df17f362e3c6
Kenichi Ishigaki <ishigaki@cpan.org>
2009-04-15 20:44:23 +0000

    DBD-SQLite: avoid tweak if dbname is ':memory:'

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -65,7 +65,7 @@ sub connect {
 
     # To avoid unicode and long file name problems on Windows,
     # convert to the shortname if the file (or parent directory) exists.
-    if ( $^O eq 'MSWin32' ) {
+    if ( $^O eq 'MSWin32' and $real ne ':memory:') {
         require Win32;
         require File::Basename;
         my ($file, $dir, $suffix) = File::Basename::fileparse($real);
