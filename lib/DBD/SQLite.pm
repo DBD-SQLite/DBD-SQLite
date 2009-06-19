404b35f224b81b3a24793df8cd3fa2d83f39cb0f
Kenichi Ishigaki <ishigaki@cpan.org>
2009-06-19 06:04:54 +0000

    DBD-SQLite: bumped up the version and updated Changes for another dev release

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -8,7 +8,7 @@ use DynaLoader ();
 use vars qw($VERSION @ISA);
 use vars qw{$err $errstr $drh $sqlite_version};
 BEGIN {
-    $VERSION = '1.26_01';
+    $VERSION = '1.26_02';
     @ISA     = 'DynaLoader';
 
     # Initialize errors
