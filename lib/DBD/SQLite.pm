385c138f67e0516ac03652cc1bca5689676e3bc7
Kenichi Ishigaki <ishigaki@cpan.org>
2009-10-06 04:36:11 +0000

    DBD-SQLite: updated version and release date

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -10,7 +10,7 @@ use vars qw{$err $errstr $drh $sqlite_version};
 use vars qw{%COLLATION};
 
 BEGIN {
-    $VERSION = '1.26_03';
+    $VERSION = '1.26_04';
     @ISA     = 'DynaLoader';
 
     # Initialize errors
