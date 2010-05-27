4a71406ea9a6eed2049994e49cdb11b34325af21
Kenichi Ishigaki <ishigaki@cpan.org>
2010-05-27 03:31:46 +0000

    version up

--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -9,7 +9,7 @@ use Test::More ();
 
 use vars qw{$VERSION @ISA @EXPORT @CALL_FUNCS};
 BEGIN {
-	$VERSION = '1.30_02';
+	$VERSION = '1.30_03';
 	@ISA     = 'Exporter';
 	@EXPORT  = qw/connect_ok dies @CALL_FUNCS/;
 
