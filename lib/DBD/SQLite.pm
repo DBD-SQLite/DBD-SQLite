03eba9808167bfdd749a2fd4f2f4688274a5759b
Adam Kennedy <adamk@cpan.org>
2011-10-21 04:34:12 +0000

    Prepare for a dev release

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -10,7 +10,7 @@ use vars qw{$err $errstr $drh $sqlite_version $sqlite_version_number};
 use vars qw{%COLLATION};
 
 BEGIN {
-    $VERSION = '1.34_01';
+    $VERSION = '1.34_02';
     @ISA     = 'DynaLoader';
 
     # Initialize errors
--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -11,7 +11,7 @@ use vars qw{$VERSION @ISA @EXPORT @CALL_FUNCS};
 my $parent;
 my %dbfiles;
 BEGIN {
-	$VERSION = '1.34_01';
+	$VERSION = '1.34_02';
 	@ISA     = 'Exporter';
 	@EXPORT  = qw/connect_ok dies dbfile @CALL_FUNCS/;
 
