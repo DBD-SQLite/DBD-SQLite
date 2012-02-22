a823c437f661d933a3bc2438a9e1d160fc5cb221
Kenichi Ishigaki <ishigaki@cpan.org>
2012-02-22 17:24:34 +0000

    updated Changes and versions

--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -11,7 +11,7 @@ use vars qw{$VERSION @ISA @EXPORT @CALL_FUNCS};
 my $parent;
 my %dbfiles;
 BEGIN {
-	$VERSION = '1.36_01';
+	$VERSION = '1.36_02';
 	@ISA     = 'Exporter';
 	@EXPORT  = qw/connect_ok dies dbfile @CALL_FUNCS/;
 
