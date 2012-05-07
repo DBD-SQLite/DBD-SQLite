29798bee02ad87e1d3a66cc7f33942b67e195a8f
Kenichi Ishigaki <ishigaki@cpan.org>
2012-05-07 05:04:20 +0000

    bumped up the version

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -10,7 +10,7 @@ use vars qw{$err $errstr $drh $sqlite_version $sqlite_version_number};
 use vars qw{%COLLATION};
 
 BEGIN {
-    $VERSION = '1.36_02';
+    $VERSION = '1.36_03';
     @ISA     = 'DynaLoader';
 
     # Initialize errors
--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -11,7 +11,7 @@ use vars qw{$VERSION @ISA @EXPORT @CALL_FUNCS};
 my $parent;
 my %dbfiles;
 BEGIN {
-	$VERSION = '1.36_02';
+	$VERSION = '1.36_03';
 	@ISA     = 'Exporter';
 	@EXPORT  = qw/connect_ok dies dbfile @CALL_FUNCS/;
 
