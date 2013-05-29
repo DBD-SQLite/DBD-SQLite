03feae789553eb6cdfdf76a53a99571bd792c260
Kenichi Ishigaki <ishigaki@cpan.org>
2013-05-29 16:12:16 +0900

    releng for 1.38_04

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -5,7 +5,7 @@ use strict;
 use DBI   1.57 ();
 use DynaLoader ();
 
-our $VERSION = '1.38_03';
+our $VERSION = '1.38_04';
 our @ISA     = 'DynaLoader';
 
 # sqlite_version cache (set in the XS bootstrap)
