f7ad52f8519d59602d333af84bf88dc0397f2810
Kenichi Ishigaki <ishigaki@cpan.org>
2013-06-10 00:12:13 +0900

    release 1.39

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -5,7 +5,7 @@ use strict;
 use DBI   1.57 ();
 use DynaLoader ();
 
-our $VERSION = '1.38_05';
+our $VERSION = '1.39';
 our @ISA     = 'DynaLoader';
 
 # sqlite_version cache (set in the XS bootstrap)
