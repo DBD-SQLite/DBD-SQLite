661d7cabac97e3aea12732d633a32490b120252e
Kenichi Ishigaki <ishigaki@cpan.org>
2013-07-28 14:28:58 +0900

    releng 1.40

--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -7,7 +7,7 @@ use Exporter   ();
 use File::Spec ();
 use Test::More ();
 
-our $VERSION = '1.39';
+our $VERSION = '1.40';
 our @ISA     = 'Exporter';
 our @EXPORT  = qw/connect_ok dies dbfile @CALL_FUNCS/;
 our @CALL_FUNCS;
