c4056c3bb82ebd8784def75b6a4bcf7345788a8e
Kenichi Ishigaki <ishigaki@cpan.org>
2014-10-23 17:19:18 +0900

    releng 1.45_02

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -5,7 +5,7 @@ use strict;
 use DBI   1.57 ();
 use DynaLoader ();
 
-our $VERSION = '1.45_01';
+our $VERSION = '1.45_02';
 our @ISA     = 'DynaLoader';
 
 # sqlite_version cache (set in the XS bootstrap)
--- lib/DBD/SQLite/VirtualTable.pm
+++ lib/DBD/SQLite/VirtualTable.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use Scalar::Util    qw/weaken/;
 
-our $VERSION = '1.45_01';
+our $VERSION = '1.45_02';
 our @ISA;
 
 
