db43f7a75e39f2f3afa894e0dff5a26070f3ac4a
Kenichi Ishigaki <ishigaki@cpan.org>
2016-01-21 10:09:37 +0900

    releng 1.49_07

--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -7,7 +7,7 @@ use Exporter   ();
 use File::Spec ();
 use Test::More ();
 
-our $VERSION = '1.49_06';
+our $VERSION = '1.49_07';
 our @ISA     = 'Exporter';
 our @EXPORT  = qw/connect_ok dies dbfile @CALL_FUNCS $sqlite_call has_sqlite requires_sqlite/;
 our @CALL_FUNCS;
