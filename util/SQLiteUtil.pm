78c097642533d9edff76290fd31d8ff9f0af8d32
Kenichi Ishigaki <ishigaki@cpan.org>
2017-01-04 00:29:05 +0900

    update SQLiteUtil to support versions released in 2017

--- util/SQLiteUtil.pm
+++ util/SQLiteUtil.pm
@@ -367,6 +367,7 @@ sub year {
   my $self = shift;
   return "snapshot" if $self->is_snapshot;
   my $version = $self->as_num;
+  return 2017 if $version >= 3160000;
   return 2016 if $version >= 3100000;
   return 2015 if $version >= 3080800;
   return 2014 if $version >= 3080300;
