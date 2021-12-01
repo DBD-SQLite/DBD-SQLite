d83bd846065fc060ba64cd2b4ba0198c467986d3
Kenichi Ishigaki <ishigaki@cpan.org>
2021-12-02 01:25:24 +0900

    Update SQLite to 3.37.0

--- lib/DBD/SQLite.pm
+++ lib/DBD/SQLite.pm
@@ -1073,7 +1073,7 @@ are limited by the typeless nature of the SQLite database.
 =head1 SQLITE VERSION
 
 DBD::SQLite is usually compiled with a bundled SQLite library
-(SQLite version S<3.36.0> as of this release) for consistency.
+(SQLite version S<3.37.0> as of this release) for consistency.
 However, a different version of SQLite may sometimes be used for
 some reasons like security, or some new experimental features.
 
